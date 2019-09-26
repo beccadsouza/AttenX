2d89a0a6c5c6a30122a5f1b0a1f3832cf583523f
Tejas Chheda <tejas4888@gmail.com>
2019-09-26 17:52:47 +0530

    Added ClassApp-Todo

--- ClassApp/views.py
+++ ClassApp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+def MakeAttention(request):
+    frames=[] # list of frames each being a numpy array image
+
+    # Eye gaze
+    # Pose
+    # Sleepy
+    # Add to DB
+
+    # Separate tracking for ==>
+    # Answering Qs - Raising hands
+    # Staring at board/teacher
+    # Solving probs
+
+    return 0
+
+def DetectAttendance(request):
+    photo=() # numpy array
+
+    # Store id
+    return 0
+
+def appInsights():
+    # Side of class less attentive
+    # Jokes - range of attention
+    return 0
