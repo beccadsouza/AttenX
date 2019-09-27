921a316e32c75716352ba614721f166a840bbe49
Tejas Chheda <tejas4888@gmail.com>
2019-09-27 18:39:39 +0530

    Sleep Detection coordinates fully complete

--- ClassApp/views.py
+++ ClassApp/views.py
@@ -15,10 +15,10 @@ def MakeAttention(request):
     curr_time = str(datetime.datetime.now().time())
 
     gaze_attn = getGazeAttention(frames[0])
-    (pose_attn,n_q,n_b,n_p) = getPoseAttention(frames[0])
-    sleep_n = getSleepNumber(frames)
+    (pose_attn, n_q, n_b, n_p) = getPoseAttention(frames[0])
+    sleep_n, sleep_coordinates = getSleepNumber(frames)
 
-    ov_attn=(gaze_attn+pose_attn)/2 - 0.1*sleep_n
+    ov_attn = (gaze_attn+pose_attn)/2 - 0.1*sleep_n
 
     obj = ClassAttention.objects.create(curr_time,gaze_attn,pose_attn,sleep_n,ov_attn,n_q,n_b,n_p)
     obj.save()
