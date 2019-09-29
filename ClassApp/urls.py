341d4258fb5d5a7083cceea6a263b95f25994858
Rebecca <beccadsouza16@gmail.com>
2019-09-29 11:52:15 +0530

    shifting urls

--- ClassApp/urls.py
+++ ClassApp/urls.py
@@ -6,8 +6,6 @@ from .ml_algo import *
 app_name = 'classapp'
 
 urlpatterns = [
-    # path('analytics1/', trial),
-    path('attendance/', DetectAttendance),
     path('<parameter1>/', get_recent_attention_percentage),
 
 ]
--- ClassroomProduct/urls.py
+++ ClassroomProduct/urls.py
@@ -6,27 +6,34 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from . import views, manipulate_frames
 from ClassApp.web_api import *
+from ClassApp.android_api import *
+from ClassApp.ml_algo import *
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
-    url(r'classapp/', include('ClassApp.urls')),
+    # url(r'classapp/', include('ClassApp.urls')),
+
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^signout/', views.signout, name='signout'),
     url(r'^home/', views.home, name='home'),
 
-    url(r'^recordattendance/', views.record_attendance, name="recordattendance"),
-    url(r'^captureface/', manipulate_frames.capture_face, name="captureface"),
+    # url(r'^recordattendance/', views.record_attendance, name="recordattendance"),
+    # url(r'^captureface/', manipulate_frames.capture_face, name="captureface"),
 
     url(r'^createsession/', views.create_session, name="createsession"),
     url(r'^streamsession/', views.stream_session, name="streamsession"),
     url(r'^qrcodegen/', views.qr_code_session, name="qrcodegen"),
     url(r'^capture/', manipulate_frames.capture_list, name="capture"),
 
+    url(r'^client/(?P<parameter1>\w+)/', get_recent_attention_percentage, name="clientaattention"),
+
+
     url(r'^sessionanalytics/(?P<parameter1>\w+)/', views.get_session_analytics, name="sessionanalytics"),
     url(r'^sessionlist/', views.get_list_session, name="sessionlist"),
-
     url(r'^graph/donutdata/', donut_data),
+
     url(r'^', RedirectView.as_view(pattern_name='home', permanent=False)),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
