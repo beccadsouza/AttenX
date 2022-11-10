75f3173a4740afeac8580faf7219d7291ab5764c
Nizor <ogbezuodenizor@yahoo.com>
2022-11-10 16:59:26 +0100

    corrected deprecated dependencies in urls

--- ClassApp/urls.py
+++ ClassApp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.conf.urls import url
+from django.urls import re_path
 from .android_api import *
 from .web_api import *
 from .ml_algo import *
 app_name = 'classapp'
 
 urlpatterns = [
-    path('<parameter1>/', get_recent_attention_percentage),
+    re_path('<parameter1>/', get_recent_attention_percentage),
 
 ]
